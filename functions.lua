4361853c6e67bb5f67c6aa819700fa39458baf89
IsSomthingWrong <43751371+IsSomthingWrong@users.noreply.github.com>
2018-12-19 20:34:09 -0600

    Delete functions.lua

--- functions.lua
+++
@@ -1,6 +0,0 @@
-function default.grow_papyrus(pos, node)
-	pos.y = pos.y - 1
-	local name = minetest.get_node(pos).name
-	if name ~= "default:dirt_with_grass" and name ~= "default:dirt" then
-		return
-	end
